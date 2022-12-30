aeb81473c18d58079c3b8733bd6fc9d540bf9796
Milind-Shende <milind.shende24@rediffmail.com>
2022-12-30 17:45:57 +0530

    train and Evaluation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="wine_quality",
+    version="0.0.1",
+    author="ineuron",
+    description="its a wine quality package",
+    author_email="milind.shende24@rediffmail.com",
+    packages = find_packages(),
+    license="MIT"
+)
